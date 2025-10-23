c1e415e5a411caf7b94592226d410d6feb8c10d5
sugirangendahayo <Sugirangendahayo835@gmail.com>
2025-10-24 00:34:45 +0200

    Finished navabr

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./*.html", // all HTML files in root
+    "./js/**/*.js", // all JS files in js folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
